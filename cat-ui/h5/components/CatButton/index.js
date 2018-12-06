988061e74ddf66f82a2d8e5fe7ad7bd43bd4e449
zhangfu <zhangfu@nasetech.com>
2018-12-06 09:57:03 +0800

    build ui

--- cat-ui/h5/components/CatButton/index.js
+++ cat-ui/h5/components/CatButton/index.js
@@ -8,16 +8,20 @@ export default class CatButton extends Component {
   static options = {
     addGlobalClass: true
   };
+  onClick() {
+    if (this.props.disabled) return;
+    this.props.onClick();
+  }
   render() {
-    const { type, size, block, className, shape } = this.props;
+    const { type, size, block, className, shape, disabled } = this.props;
     const sizeCls = size ? `cat-btn__${size}` : '';
     const typeCls = type ? `cat-btn__${type}` : '';
-    const isBlockCls = block ? `cat-btn__is-block}` : '';
-    const formClassNames = `cat-btn ${sizeCls} ${typeCls} ${isBlockCls} ${className} cat-btn__${shape}`;
+    const isBlockCls = block ? 'cat-btn__is-block' : '';
+    const disabledCls = disabled ? 'cat-btn__is-disabled' : '';
+    const formClassNames = `cat-btn ${sizeCls} ${typeCls} ${isBlockCls} ${className} cat-btn__${shape} ${disabledCls}`;
 
     const {
       plain,
-      disabled,
       loading,
       formType,
       openType,
@@ -30,7 +34,9 @@ export default class CatButton extends Component {
       onGetPhoneNumber,
       lang
     } = this.props;
-    return <Button className={formClassNames} plain={plain} disabled={disabled} loading={loading} formType={formType} openType={openType} appParameter={appParameter} hoverClass={hoverClass} hoverStopPropagation={hoverStopPropagation} hoverStartTime={hoverStartTime} hoverStayTime={hoverStayTime} onGetUserInfo={onGetUserInfo} onGetPhoneNumber={onGetPhoneNumber} lang={lang} onClick={this.props.onClick}>
+    return <Button className={formClassNames} plain={plain}
+    // disabled={disabled}
+    loading={loading} formType={formType} openType={openType} appParameter={appParameter} hoverClass={hoverClass} hoverStopPropagation={hoverStopPropagation} hoverStartTime={hoverStartTime} hoverStayTime={hoverStayTime} onGetUserInfo={onGetUserInfo} onGetPhoneNumber={onGetPhoneNumber} lang={lang} onClick={this.onClick}>
         {!loading && this.props.renderPrefix}
         {this.props.children}
         {this.props.renderSuffix}
